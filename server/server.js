07de8914730571b73e6afb11012a01c483dc4299
cal <callummac@protonmail.com>
2018-11-11 21:18:21 +0000

    fixes for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 // const mongoose = require('mongoose');
 
