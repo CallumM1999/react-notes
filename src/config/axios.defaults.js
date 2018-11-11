ac1f9bdf51a40eb33a8d98a80a26acfd703dc2a3
cal <callummac@protonmail.com>
2018-11-11 21:39:12 +0000

    mmm

--- src/config/axios.defaults.js
+++ src/config/axios.defaults.js
@@ -1,3 +1,4 @@
 module.exports = {
-    baseURL: process.env.API_PATH || 'http://localhost:3000',
+    baseURL: 'https://agile-mountain-99443.herokuapp.com',
+    // baseURL: 'http://localhost:3000',
 }
