1e305e5b3865146cbc85772f9043e94c4a53adab
cal <callummac@protonmail.com>
2018-11-13 17:40:08 +0000

    forgot to update API url

--- src/config/axios.defaults.js
+++ src/config/axios.defaults.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // baseURL: 'https://agile-mountain-99443.herokuapp.com',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://agile-mountain-99443.herokuapp.com',
+    // baseURL: 'http://localhost:3000',
 }
